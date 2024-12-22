ad98cdb35a1a5de0e194c1c898d39b0e2324ac17
a107329 <a107329@alunos.uminho.pt>
2024-12-22 18:36:26 +0000

    Inicio do trabalho do historico

---
+++ trabalho-pratico/src/querie/query4.c
@@ -0,0 +1,18 @@
+#include "controler/artistsController.h"
+#include "controler/historyController.h"
+#include "controler/musicsController.h"
+#include "utilidades.h"
+#include "Entitys/artists.h"
+#include "Entitys/history.h"
+#include "Entitys/musics.h"
+#include "Output.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <unistd.h>
+
+/* Quert 4 ideias
+Ordenar o historico 
+
+*/
